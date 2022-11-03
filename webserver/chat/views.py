434169c94ad8496d66211808d794f8955ce1edb7
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-11-03 19:53:39 +0530

    Add basic setup for websockets room

---
+++ webserver/chat/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'chat/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
